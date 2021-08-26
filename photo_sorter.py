84be2101257066467d391f2134d2be22adf80d2b
slammywill123 <samwillemsofficial@gmail.com>
2021-08-26 16:32:55 +1200

    Updated draw funct

--- photo_sorter.py
+++ photo_sorter.py
@@ -1,11 +1,29 @@
+"""Sorting ++ Aplication.
+
+Sorts photos based on their metadata.
+
+Author: Sam Willems
+"""
+
 import tkinter as tk
 from tkinter import filedialog
 import os
 from PIL import Image
 from PIL.ExifTags import TAGS
+from pyglet import *
+
 
 root = tk.Tk() # Initializes Tkinter window for file selection.
 root.withdraw() # Hides window until get_file_path function is called.
+
+
+# Pyglet initialisation for GUI.
+WINDOW_SIZE = (400, 550)
+TITLE = 'Sorting ++'
+WINDOW = window.Window(*WINDOW_SIZE, TITLE)
+resource.path = ["resources"]
+resource.reindex()
+
 
 IMAGE_TYPES = ['png', 'jpg', 'tiff','jpeg']
 
@@ -38,7 +56,11 @@ def check_if_image(image):
 
 
 def get_image_list(file_path):
-    """Returns all of the images in the directory."""
+    """Returns all of the images in the directory.
+
+    Args:
+        file_path (string): The path to the file containing the images.
+    """
     files = os.listdir(file_path)
     images = [file for file in files if check_if_image(file)]
     return images
@@ -79,16 +101,35 @@ def sort_name(image_list):
     return sorted_list
 
 
+def draw():
+    batch = graphics.Batch() # Groups all images / shapes to draw at once.
+    background = sprite.Sprite(img=resource.image("background.png"), x=0, y=0, batch=batch)
+    batch.draw()
+
+
+
+@WINDOW.event
+def on_draw():
+    """Renders the pyglet window.
+    """
+    WINDOW.clear()
+    draw()
+
+
 def main():
     """Main."""
-    from_path = get_file_path()
-    image_list = get_image_list(from_path)
-    print(image_list)
-    for image in image_list:
-        data = get_image_data(f"{from_path}/{image}")
-        print(data)
-    to_path = get_photo_location_path()
-    print(sort_name(image_list))
+    app.run() # Run pyglet window.
+    # from_path = get_file_path()
+    # image_list = get_image_list(from_path)
+    # print(image_list)
+    # for image in image_list:
+    #     data = get_image_data(f"{from_path}/{image}")
+    #     print(data)
+    # to_path = get_photo_location_path()
+    # print(sort_name(image_list))
 
 
 main()
+
+
+# TODO: Add error exception for file path.
